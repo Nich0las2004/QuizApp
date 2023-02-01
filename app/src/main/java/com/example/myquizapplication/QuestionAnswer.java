dce1ee6dc1da469f0b87dca0b3b69aa87f0c87b0
Nika <nikaoniani2018@gmail.com>
2023-02-01 19:59:21 +0400

    QuizApp

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Who is the Ceo of Tesla?",
+            "Who founded Apple?",
+            "Which language is used in android studio?"
+    };
+
+    public static String choices[][] ={
+            {"Musk","Bezos","Zuckerberg","Cook"},
+            {"Gates","Jobs","Cook","Pichai"},
+            {"Java","Javascript","Python","Ruby"}
+    };
+
+    public static String correctAnswers[] ={
+            "Musk",
+            "Jobs",
+            "Java"
+    };
+}
